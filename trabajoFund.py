7a1b58f7f47d8268764f14b5d40d9c0009f14b31
Ivankajavi <iv.lavin@duocuc.cl>
2025-04-11 10:01:18 -0400

    Add files via upload
    Signed-off-by: Ivankajavi <iv.lavin@duocuc.cl>

---
+++ trabajoFund.py
@@ -0,0 +1,42 @@
+#VariableInt = 10
+#variablefloat = 15.2 
+#VariableStr = "hola"
+#VariableBool = True
+
+# Ejercicio:
+# nombre = input("dime tu nombre: ")
+# print ("bienvenida, ", nombre)
+
+# NumEntero = int(input("Ingresa un numero entero: "))
+# NumDecimal = float(input("Dime un decimal: "))
+
+# operacion:
+# Resultado = NumEntero + NumDecimal
+
+# salida:
+# print("El resultado es: ", Resultado)
+
+NumEntero = int(input("Dime un numero: "))
+NumEnter2 = int(input("Dime otro numero: "))
+
+Resultado = NumEntero + NumEnter2
+print("El resultado de la suma es: ", Resultado)
+
+NumEntero = int(input("Dime un numero: "))
+NumEnter2 = int(input("Dime otro numero: "))
+
+Resultado = NumEntero - NumEnter2
+print("El resultado de la suma es: ", Resultado)
+
+NumEntero = int(input("Dime un numero: "))
+NumEnter2 = int(input("Dime otro numero: "))
+
+Resultado = NumEntero * NumEnter2
+print("El resultado de la suma es: ", Resultado)
+
+NumEntero = int(input("Dime un numero: "))
+NumEnter2 = int(input("Dime otro numero: "))
+
+Resultado = NumEntero / NumEnter2
+print("El resultado de la suma es: ", Resultado)
+
