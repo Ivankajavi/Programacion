7e449de1c0bb628f5d18c6bb2096ed582a189a7a
Ivankajavi <iv.lavin@duocuc.cl>
2025-04-14 11:22:20 -0400

    Add files via upload
    Signed-off-by: Ivankajavi <iv.lavin@duocuc.cl>

---
+++ clase 5.py
@@ -0,0 +1,50 @@
+#tienda de perfumes blogger
+#el representante de ventas de la tienda necesita organizar el formulario de ventas
+#el piensa, que representando el siguiente formulario
+#podria mejorar sus ventas ya que el formulario tiene 10 items para atender
+#a continuacion los cargos que desea implementar:
+
+#numero de boleta/factura
+#Rut
+#nombre cliente
+#Productos del pedido
+#10 productos dentro del inventario:
+a = """
+♥ Perfume damas♥ :
+1. Perfume organza
+2. Perfume katy perry
+3. Mañana fresca
+4. La mejor noche
+5. Sueño intrusivo
+
+♥ Perfume hombre ♥ :
+6. Ahora si voy  
+7. Antonio bandera
+8. Lacoste
+9. Hugo boss
+10. a que te quito el sueño
+"""
+#todos los de 50ML estan a $10 lks
+#todos los de 100ML estan a $18 lks
+#añadir el pedido, sacar el total y sacar iva
+
+print("Bienvenido a la tienda Blogger")
+
+nombre = input("Dime tu nombre: ")
+rut = input("Dime tu rut sin guión: ")
+
+print("Selecciona el perfume a escoger")
+
+print(a)
+Perfumeescogido = input("Ingresa el número del perfume que escogiste: ")
+
+Elección = ()
+
+print("El perfume que escogiste tiene 2 formatos, ¿Cuál deseas escoger?")
+print("Opcion 1: 50 ML $10.000")
+print("Opcion 2: 100 ML $18.000")
+
+
+
+
+
