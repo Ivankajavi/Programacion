4b8add8c51a2b42d9bc9ce0fde88060445f882b5
Ivankajavi <iv.lavin@duocuc.cl>
2025-04-21 10:59:57 -0400

    Add files via upload
    Signed-off-by: Ivankajavi <iv.lavin@duocuc.cl>

---
+++ clase6.py
@@ -0,0 +1,33 @@
+# Edad = int(input("Dime tu edad: "))
+
+# if Edad >= 18:
+    #print("eres mayor de edad")
+# else:
+     #print("eres menor de edad")
+
+#nota = float(input("Dime tus notas: "))
+
+#if nota >= 6.0:
+    #print("aprobado")
+#elif nota <= 5.0:
+    #print("bueno")
+#elif nota >= 4.0:
+    #print("debes mejorar")
+#else:
+    #print("reprobado")
+
+#contador = 1
+
+#while contador <= 5:
+    #print(f"repeticion: {contador}")
+    #contador += (1)
+
+# For
+#for i in range(1, 11):
+    #print(f"Valor: {i}")
+
+#Funcion
+def saludo():
+    print("holi, bienvenido")
+    
+saludo ()
