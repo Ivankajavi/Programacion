5e308deaaf3acb253c284ca5c34ff53cf2c11c02
Ivankajavi <iv.lavin@duocuc.cl>
2025-04-23 09:35:33 -0400

    Add files via upload
    Signed-off-by: Ivankajavi <iv.lavin@duocuc.cl>

---
+++ clase7.py
@@ -0,0 +1,12 @@
+#variable = 10
+#variableFloat = 1.2
+#variablestr = "hola"
+#VariableBool = true
+
+dato = input("ingresa un numero: ")
+print("Tipo de datos recibido: ", type(dato))
+
+numero = int(dato)
+resultado = numero + 10
+print("resultado: ", resultado)
+
