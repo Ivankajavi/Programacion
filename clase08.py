77f1f8c85251664cd20dc5bbaba41bba8902384c
Ivankajavi <iv.lavin@duocuc.cl>
2025-04-25 11:23:24 -0400

    Add files via upload
    Signed-off-by: Ivankajavi <iv.lavin@duocuc.cl>

---
+++ clase08.py
@@ -0,0 +1,26 @@
+#def suma(a, b):
+    #return a + b
+
+#print("vamos a hacer una suma: ")
+
+#a = int(input("dime un numero: "))
+#b = int(input("dime un numero: "))
+
+#resultado = suma(a, b)
+#print("el resultado es: ", resultado)
+
+#realizar un programa que le solicite 2 numeros a un usuario
+#un a y un b, y en base a estos 2 numeros sacaremos un
+#numero aleatorio, luego al resultado se le suma o resta el
+#10% 
+
+def suma(a, b):
+    return a + b
+
+print("")
+
+import random
+resultado = random.randint(int(a), int(b))
+print("")
+
+
